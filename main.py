5af659c404443f9c3e560ac01bde7b863bf0fc64
Static-Name <64046097+GAMPO-al@users.noreply.github.com>
2021-04-03 22:20:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def Findme(liste):
+def Find_Duplicate(liste):
     new_liste = []
     result = {}
     for i in range(len(liste)):
@@ -8,4 +8,4 @@ def Findme(liste):
             new_liste.append(liste[i])
     return "The Duplicate numbers are "+ str(result.keys())[9:]
 x = [1,5,7,44,76,5,7,45,22,121,44,5]
-print(Findme(x))
+print(Find_Duplicate(x))
