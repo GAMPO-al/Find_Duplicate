a6b6fd99897ab4a73e20072dfe4718652481027a
Static-Name <64046097+GAMPO-al@users.noreply.github.com>
2021-04-04 13:40:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def Find_Duplicate(liste):
     new_liste = []
     result = {}
-    for i in range(len(liste)):
-        if liste[i] in new_liste:
-            result[liste[i]] = 1
+    for item in liste:
+        if item in new_liste:
+            result[item] = 1
         else:
-            new_liste.append(liste[i])
-    return "The Duplicate numbers are "+ str(result.keys())[9:]
-x = [1,5,7,44,76,5,7,45,22,121,44,5]
+            new_liste.append(item)
+    return "The Duplicate numbers are " + str(result.keys())[9:]
+x = [1, 5, 7, 44, 76, 5, 7, 45, 22, 121, 44, 5]
 print(Find_Duplicate(x))
